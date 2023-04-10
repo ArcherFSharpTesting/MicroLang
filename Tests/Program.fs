dc9e4e288efd6371cd8501e56a570dad46ad0bcc
jason-kerney <jason.kerney@edf-re.com>
2023-04-10 11:40:25 -0700

    chore: updated BOw

--- Tests/Program.fs
+++ Tests/Program.fs
@@ -8,23 +8,18 @@ open Archer.MicroLang.Lang
 let framework = bow.Framework ()
 
 // These tests test the testing environment used to test the framework
-let testDoublesTests =
-    [
-        ``UnitTest Base Case``.``Test Cases``
-        ``UnitTestExecutor Happy Path``.``Test Cases``
-        ``UnitTestExecutor With a Failing Test``.``Test Cases``
-        ``UnitTestExecutor StartExecution``.``Test Cases``
-        ``UnitTestExecutor StartSetup``.``Test Cases``
-        ``UnitTestExecutor EndSetup``.``Test Cases``
-        ``UnitTestExecutor StartTest``.``Test Cases``
-        ``UnitTestExecutor EndTest``.``Test Cases``
-        ``UnitTestExecutor StartTearDown``.``Test Cases``
-        ``UnitTestExecutor EndExecution``.``Test Cases``
-    ]
-    |> List.concat
-
-testDoublesTests
+[
+    ``UnitTest Base Case``.``Test Cases``
+    ``UnitTestExecutor Happy Path``.``Test Cases``
+    ``UnitTestExecutor With a Failing Test``.``Test Cases``
+    ``UnitTestExecutor StartExecution``.``Test Cases``
+    ``UnitTestExecutor StartSetup``.``Test Cases``
+    ``UnitTestExecutor EndSetup``.``Test Cases``
+    ``UnitTestExecutor StartTest``.``Test Cases``
+    ``UnitTestExecutor EndTest``.``Test Cases``
+    ``UnitTestExecutor StartTearDown``.``Test Cases``
+    ``UnitTestExecutor EndExecution``.``Test Cases``
+]
+|> List.concat
 |> framework.AddTests
-
-framework
 |> runAndReport
